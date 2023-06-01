68cf498b1b3635a7c455c985dcee680402748fee
SadmanJawad <jawadnabil977@gmail.com>
2023-06-01 16:02:06 +0600

    Display All users and Create Make admin API

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixkqk3t.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,16 +27,61 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users")
     const menuCollection = client.db("bistroDb").collection("menu")
     const reviewsCollection = client.db("bistroDb").collection("reviews")
     const cartCollection = client.db("bistroDb").collection("carts")
+  
+    // users related api
+    app.get('/users', async (req, res) => {
+        const result = await usersCollection.find().toArray()
+        res.send(result)
+    })
 
+
+
+
+
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        
+        const query = { email : user.email };
+        const existingUser = await usersCollection.findOne(query);
+        
+        if(existingUser){
+          return res.send({status: 'error', message: 'User already exists'});
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    })
+
+    app.patch('users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+     const filter = { _id : new ObjectId(id) };
+      const updateDoc = {
+      $set: {
+        role : 'admin'
+      },
+    };
+
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+    
+
+
+    })
+
+
+
+
+// menu related api
     app.get('/menu', async (req, res) => {
     const result = await menuCollection.find().toArray()
     res.send(result)
     
     })
-
+// reviews related api
     app.get('/reviews', async (req, res) => {
         const result = await reviewsCollection.find().toArray();
         res.send(result);
@@ -57,6 +102,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 

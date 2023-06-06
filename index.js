6c3e44d23587215a4d6fc46262b752d20ffa4ff4
SadmanJawad <jawadnabil977@gmail.com>
2023-06-07 00:28:25 +0600

    create payment api and send payment data to server and save payment info to the database

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu")
     const reviewsCollection = client.db("bistroDb").collection("reviews")
     const cartCollection = client.db("bistroDb").collection("carts")
+    const paymentCollection = client.db("bistroDb").collection("payments")
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
@@ -181,7 +182,7 @@ async function run() {
     })
 
     // create payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent',verifyJWT, async (req, res) => {
       const {price} = req.body;
       const amount = price * 100;
       const paymentIntent = await stripe.paymentIntents.create({
@@ -194,6 +195,17 @@ async function run() {
     })
   })
 
+
+  // payment related api
+  app.post('/payments', verifyJWT ,async (req, res) => {
+    const payment = req.body;
+    const insertResult = await paymentCollection.insertOne(payment);
+    
+    const query = { _id : { $in : payment.cartItems.map(id => new ObjectId(id)) } };
+    const deleteResult = await cartCollection.deleteMany(query);
+
+    res.send({insertResult, deleteResult});
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
